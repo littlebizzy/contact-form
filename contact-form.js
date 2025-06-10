1edaf407d87747c36346b7a2bb7464a3a3bb657d
Jesse Nickles <jesse@littlebizzy.com>
2025-06-11 03:55:53 +0700

    Update and rename store-contact-form.js to contact-form.js

--- contact-form.js
+++ contact-form.js
@@ -1,9 +1,9 @@
 jQuery( function ( $ ) {
-	$( '#store-contact-form' ).on( 'submit', function ( event ) {
+	$( '#contact-form' ).on( 'submit', function ( event ) {
 		event.preventDefault();
 
 		var $form = $( this );
-		var $response = $( '#store-contact-response' );
+		var $response = $( '#contact-form-response' );
 
 		// enforce HTML5 field validation before ajax
 		if ( ! $form[0].checkValidity() ) {
@@ -14,7 +14,7 @@ jQuery( function ( $ ) {
 		var formData = $form.serialize();
 		$response.text( 'Sending...' );
 
-		$.post( storeContactForm.ajax_url, formData, function ( res ) {
+		$.post( contactForm.ajax_url, formData, function ( res ) {
 			if ( res.success ) {
 				$response.text( res.data );
 				$form[0].reset();
